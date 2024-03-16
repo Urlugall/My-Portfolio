5746d276adda29b81861b475ad4629bb79b50948
Illia <127438213+Urlugall@users.noreply.github.com>
2024-03-17 00:33:49 +0100

    Cloudflare Build Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1904cd0a94a2ce56"; }
+/******/ 		__webpack_require__.h = function() { return "72bf9aaa8960a9ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

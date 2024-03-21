742503f74b09a6c77907f9e6766b2351a86612fd
Illia <127438213+Urlugall@users.noreply.github.com>
2024-03-21 14:05:28 +0100

    Test Upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const MainPage: NextPage = () => {
       <NavigationPanel />
       
       <div className="page-content absolute bottom-0 right-0 text-right w-1/2 p-6 sm:p-8 md:p-10 lg:p-14">
-        <p className="regular-text">Hope you will find what you need on this page. I develop a variety of mobile & desktop games and programs, thus increasing the number of my possibilities. Also, I try to keep pace with creativity almost every day, which is like my second weapon.</p>
+        <p className="regular-text">Hope you will find what you need on this page! I develop a variety of mobile & desktop games and programs, thus increasing the number of my possibilities. Also, I try to keep pace with creativity almost every day, which is like my second weapon.</p>
       </div>
     </div>
   );
